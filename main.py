d9c4183f644e85f253137f30886b49b7a9324578
IrinIv <iryna.ivanova2@gmail.com>
2021-07-15 13:15:25 -0700

    ex3 commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import requests
+#import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+#response = requests.get("https://playground.learnqa.ru/api/get_text")
+#print(response.text)
+
+print("Hello from Irina")
