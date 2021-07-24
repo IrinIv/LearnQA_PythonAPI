b03378e7921028e2a3722ad57270a02c61f75b96
IrinIv <iryna.ivanova2@gmail.com>
2021-07-24 08:49:47 -0700

    created and updated tokens.py

--- tokens.py
+++ tokens.py
@@ -1,4 +1,29 @@
+import json
 import requests
+import time
 
-response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
-print(response.text)
+
+def findSum(a, b):
+
+    c = a + b
+
+    payload = {'a': {a}, 'b': {b}}
+    response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params=payload)
+    token = json.loads(response.text)['token']
+
+    sum = a + c
+
+    payload = {'token': token}
+    response1 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params=payload)
+    print(response1.json())
+
+    time.sleep(10)
+
+    payload = {'token': token}
+    response2 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params=payload)
+    print(response2.json())
+
+    return sum
+
+
+print(findSum(160755, 37459678))
