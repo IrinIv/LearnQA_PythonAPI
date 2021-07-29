57b3fb3c1fe39bb55d02451f7631077f52f5715f
IrinIv <iryna.ivanova2@gmail.com>
2021-07-28 17:01:02 -0700

    Update ex10.py

--- tests/ex10.py
+++ tests/ex10.py
@@ -1,10 +1,6 @@
-
 def test_phrase():
     phrase = input("Set a phrase: ")
-    expected_result = (int(len(phrase)))
+    actual_result = (int(len(phrase)))
     print(int(len(phrase)))
 
-
-    assert expected_result < 15, "The phrase is greater than 15 characters"
-
-
+    assert actual_result < 15, "The phrase is greater than 15 characters"
