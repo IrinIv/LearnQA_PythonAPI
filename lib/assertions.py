79481bde94e25726ab91441b671f88036ffef889
IrinIv <iryna.ivanova2@gmail.com>
2021-08-03 19:38:32 -0700

    created a few tests

--- lib/assertions.py
+++ lib/assertions.py
@@ -26,4 +26,22 @@ class Assertions:
         assert response.status_code == expected_status_code, \
         f"Unexpected status code. Expected: {expected_status_code}. Actual: {response.status_code}"
 
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecoderError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}"
 
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}', but it's present"
+
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecoderError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
