7d0cb3c000faeaa0ca34e36f7ea1d2e169aa43d2
IrinIv <iryna.ivanova2@gmail.com>
2021-08-05 12:40:55 -0700

    Update test_user_edit.py

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -25,27 +25,101 @@ class TestUserEdit(BaseCase):
 
         response2 = MyRequests.post("/user/login", data=login_data)
 
-        auth_sid = self.get_cookie(response2,"auth_sid")
+        auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
 
         # EDIT
         new_name = "Changed name"
         response3 = MyRequests.put(f"/user/{user_id}",
-                                 headers={"x-csrf-token": token},
-                                cookies={"auth_sid": auth_sid},
-                                 data={"firstName": new_name}
-        )
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
 
         Assertions.assert_code_status(response3, 200)
 
         # GET
         response4 = MyRequests.get(f"/user/{user_id}",
-                    headers={"x-csrf-token": token},
-                    cookies={"auth_sid": auth_sid}
-        )
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid}
+                                   )
 
         Assertions.assert_json_value_by_name(
             response4,
             "firstName",
             new_name,
             "Wrong name of the user after edited")
+
+    def test_edit_not_authorized_user(self):
+        new_name = "Changed name"
+
+        response = MyRequests.put(f"/user/6572", data={"firstName": new_name})
+
+        Assertions.assert_code_status(response, 400)
+
+    def test_edit_user_details_auth_as_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+
+        # new user was registered right before this test
+        new_user_id = 6575
+
+        new_name = "Changed name"
+        response1 = MyRequests.put(f"/user/{new_user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response1, 400)
+
+    def test_edit_wrong_email_format(self):
+        data = {
+            'email': 'learnqa08052021122142@example.com',
+            'password': '123'
+        }
+
+        response = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+
+        new_email = 'learnqa08052021122142example.com'
+
+        response1 = MyRequests.put(f"/user/login",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"email": new_email}
+                                   )
+
+        Assertions.assert_code_status(response1, 400)
+        assert response1.text == "Wrong HTTP method"
+
+    def test_edit_user_with_short_firstName(self):
+        data = {
+            'email': 'learnqa08052021122142@example.com',
+            'password': '123'
+        }
+
+        response = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+
+        new_firstName = 'l'
+
+        response1 = MyRequests.put(f"/user/login",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_firstName}
+                                   )
+
+        Assertions.assert_code_status(response1, 400)
+        assert response1.text == "Wrong HTTP method"
