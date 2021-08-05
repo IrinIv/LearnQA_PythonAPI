d5564fd663813425025884f75db9d4c2bb593542
IrinIv <iryna.ivanova2@gmail.com>
2021-08-05 11:42:19 -0700

    Update test_user_get.py

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -31,6 +31,30 @@ class TestUserGet(BaseCase):
 
         Assertions.assert_json_has_keys(response2, expected_fields)
 
+    def test_get_user_details_auth_as_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response2 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # new user was registered right before this test
+        new_user_id = 6572
+
+        response3 = MyRequests.get(f"/user/{new_user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+
+        Assertions.assert_code_status(response3, 200)
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_no_key(response3, "email")
+        Assertions.assert_json_has_no_key(response3, "firstName")
+        Assertions.assert_json_has_no_key(response3, "lastName")
+
 
 
 
